5dc4a658ba40658c1765f1d85350f5197a3dc266
Phuonganh Vu <phuongav@white-flipper.cs.pdx.edu>
2012-11-14 13:02:32 -0800

    Update main test

--- tests/test_tiptop_main.py
+++ tests/test_tiptop_main.py
@@ -35,21 +35,21 @@ def main():
         subprocess.Popen(base_oldcmd + output + ".old", stdout=subprocess.PIPE, shell=True)
         subprocess.Popen(base_newcmd + output + ".new", stdout=subprocess.PIPE, shell=True)
                 
-        #Examine old file
-        tiptop_output = open (output + ".old")  
-        for line in tiptop_output:
-            for elem in objectlist:
-                if elem in line:
-                    objectoldmap [elem] = line.split()
-        tiptop_output.close()
+    #Examine old file
+    tiptop_output = open (output + ".old")  
+    for line in tiptop_output:
+        for elem in objectlist:
+            if elem in line:
+                objectoldmap [elem] = line.split()
+    tiptop_output.close()
         
-        #Examine new file
-        tiptop_output = open (output + ".new")  
-        for line in tiptop_output:
-            for elem in objectlist:
-                if elem in line:
-                    objectnewmap [elem] = line.split()
-        tiptop_output.close()
+    #Examine new file
+    tiptop_output = open (output + ".new")  
+    for line in tiptop_output:
+        for elem in objectlist:
+            if elem in line:
+                objectnewmap [elem] = line.split()
+    tiptop_output.close()
 
     
     suite = unittest.TestLoader().loadTestsFromTestCase(test_tiptop_fd)
