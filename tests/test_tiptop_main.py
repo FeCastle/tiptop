8bca2d24fb84590841db297fabe38d3ef61fd5bb
Phuonganh Vu <phuongav@chatham.cs.pdx.edu>
2012-11-14 18:48:19 -0800

    Update main

--- tests/test_tiptop_main.py
+++ tests/test_tiptop_main.py
@@ -1,20 +1,32 @@
+"""
+Usage: python test_tiptop_main.py [pass]
+pass - Used when user already configure commands run in background.
+Need to put list of commands in testobjects.txt before running this script
+"""
+from sys import argv
 from test_tiptop_fd import test_tiptop_fd
 import unittest
 import subprocess
 import cPickle as pickle
 
 def main():   
-    
-    old_tiptop = raw_input ("Enter relative path to old tiptop (eg ../src/tiptop): ")
-    new_tiptop = raw_input ("Enter relative path to new tiptop: ")
-    
-    base_oldcmd = old_tiptop + " -b -c -n 2 -o "
-    base_newcmd = new_tiptop + " -b -c -n 2 -o "
     processlist = []
     outputlist = []
     objectlist = []
     objectoldmap = {}
     objectnewmap = {}
+    
+    old_tiptop = raw_input ("Enter relative path to old tiptop (eg ../src/tiptop): ")
+    new_tiptop = raw_input ("Enter relative path to new tiptop: ")
+    
+    if old_tiptop == '':
+        old_tiptop = "../../oldtiptop/tiptop/src/tiptop"
+    if new_tiptop == '':
+        new_tiptop = "../src/tiptop"
+    
+    #build tiptop command
+    base_oldcmd = old_tiptop + " -b -c -n 2 -o "
+    base_newcmd = new_tiptop + " -b -c -n 2 -o "    
     
     #read testobjects.txt and build list of commands
     try:
@@ -27,9 +39,10 @@ def main():
     except IOError:
         print "Missing testobjects.txt"
     
-    #Run commands in background 
-    for cmd in processlist:
-        subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    #Run commands in background like firefox, google-chrome, skip if invoked with 'pass'
+    if argv[1] != 'pass':
+        for cmd in processlist:
+            subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
  
     #use tiptop to evaluate info
     for output in outputlist:
@@ -52,8 +65,10 @@ def main():
                 objectnewmap [elem] = line.split()
     tiptop_output.close()
 
+    #dump info to file for test suite to read
     pickle.dump((objectoldmap, objectnewmap), open( "tiptop_test.obj", "wb"))
     
+    #Run test suite
     suite = unittest.TestLoader().loadTestsFromTestCase(test_tiptop_fd)
     unittest.TextTestRunner(verbosity=2).run(suite)
     
