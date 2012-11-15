9575ced505c8383c2db9a82bfda7e8bf2e93f4e1
Phuonganh Vu <phuongav@galapagos.cs.pdx.edu>
2012-11-14 17:08:52 -0800

    Update fd

--- tests/test_tiptop_fd.py
+++ tests/test_tiptop_fd.py
@@ -1,34 +1,36 @@
-from sys import argv
-
 """
 Usage: python test_tiptop_fd.py 
 """
-import random
 import unittest
+import cPickle as pickle
 
 class test_tiptop_fd (unittest.TestCase):
     """
     Test Tiptop File descriptor limits
     """
 
-    def setUp(self):        
-        self.seq = range(10)
+    def setUp(self):      
+        try:
+            (self.map1, self.map2) = pickle.load (open( "tiptop_test.obj", "rb"))
+        except IOError:
+            print "Could not find tiptop data file tiptop_test.obj" 
 
-    def test_compare(self, map1, map2):
-        objectlist = sorted(map1.keys())        
+    def test_compare(self):
+        objectlist = sorted(self.map1.keys())        
         for item in objectlist:
-            self.assertEqual (map1[item], map2[item])
+            self.assertEqual (self.map1[item], self.map2[item])
         
-    def test_findquestionmark(self, map2):
-        objectlist = sorted(map2.keys())        
+    def test_findquestionmark(self):
+        objectlist = sorted(self.map2.keys())        
         for item in objectlist:
-            self.assertNotIn('?', map2[item], "Found no ?")
+            self.assertNotIn('?', self.map2[item], "Found no ? for %s" % item)
+            print self.map2[item]
 
-    def test_sample(self):
-        with self.assertRaises(ValueError):
-            random.sample(self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
+    def suite(self):
+        suite = unittest.TestSuite()
+        suite.addTest(test_tiptop_fd('test_compare'))
+        suite.addTest(test_tiptop_fd('test_findquestionmark'))
+        return suite
 
 #if __name__ == '__main__':
 #    unittest.main()
