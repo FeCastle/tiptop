38cacd5aabccaa58196a55e1081dfb5bc627d93c
Phuonganh Vu <phuongav@rita.cat.pdx.edu>
2012-11-16 13:40:46 -0800

    Update fd

--- tests/test_tiptop_fd.py
+++ tests/test_tiptop_fd.py
@@ -1,6 +1,3 @@
-"""
-Usage: python test_tiptop_fd.py 
-"""
 import unittest
 import cPickle as pickle
 
@@ -15,10 +12,10 @@ class test_tiptop_fd (unittest.TestCase):
         except IOError:
             print "Could not find tiptop data file tiptop_test.obj" 
 
-    def test_found(self):       
+    def test_got_entry(self):       
         self.assertIsNotNone(self.map1, "Found no entry for objects")            
     
-    def test_comparequestionmark(self):
+    def test_compare_questionmark(self):
         objectlist = sorted(self.map1.keys())               
         for item in objectlist:
             i = 0
@@ -29,12 +26,11 @@ class test_tiptop_fd (unittest.TestCase):
                     self.assertNotEqual('?', self.map2[item][i], "Found ? for both tiptop")
                     oldresult.append(self.map1[item][i])
                     newresult.append(self.map2[item][i])
-                    #print "for %s, %s becomes %s" % (item, self.map1[item][i], self.map2[item][i])
                 i = i + 1
             if oldresult != []:
                 print "for %s, %s becomes %s" % (item, ' '.join(oldresult), ' '.join(newresult))
     
-    def test_comparedash(self):
+    def test_compare_dash(self):
         objectlist = sorted(self.map1.keys())        
         for item in objectlist:
             i = 0
@@ -45,22 +41,21 @@ class test_tiptop_fd (unittest.TestCase):
                     self.assertNotEqual('-', self.map2[item][i], "Found - for both tiptop")
                     oldresult.append(self.map1[item][i])
                     newresult.append(self.map2[item][i])
-                    #print "for %s, %s becomes %s" % (item, self.map1[item][i], self.map2[item][i])
                 i = i + 1
             if oldresult != []:
                 print "for %s, %s becomes %s" % (item, ' '.join(oldresult), ' '.join(newresult))
         
-    def test_findquestionmark(self):
+    def test_find_questionmark(self):
         objectlist = sorted(self.map2.keys())        
         for item in objectlist:
             self.assertNotIn('?', self.map2[item], "Found ? for %s" % item)
 
     def suite(self):
         suite = unittest.TestSuite()
-        suite.addTest(test_tiptop_fd('test_found'))
-        suite.addTest(test_tiptop_fd('test_comparequestionmark'))
-        suite.addTest(test_tiptop_fd('test_comparedash'))
-        suite.addTest(test_tiptop_fd('test_findquestionmark'))
+        suite.addTest(test_tiptop_fd('test_got_entry'))
+        suite.addTest(test_tiptop_fd('test_compare_questionmark'))
+        suite.addTest(test_tiptop_fd('test_compare_dash'))
+        suite.addTest(test_tiptop_fd('test_find_questionmark'))
         return suite
 
 #if __name__ == '__main__':
